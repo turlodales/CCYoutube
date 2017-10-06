d53cf1749993fc690d26c39e5efcbf9225cbfb7f
venkata surya <engineerr.hope@gmail.com>
2017-10-06 07:57:58 -0700

    Initial Commit

---
+++ CCYoutube/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CCYoutube
+//
+//  Created by surya on 10/6/17.
+//  Copyright © 2017 surya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CCYoutube/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCYoutube
+//
+//  Created by surya on 10/6/17.
+//  Copyright © 2017 surya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
